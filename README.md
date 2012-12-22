feb5d266d3d25e5167338a66a29f5da9d2c69508
Jonathan Pallant <github@thejpster.org.uk>
2012-12-22 17:49:48 +0000

    Add option to use arm-none-eabi toolchain rather than Ubuntu's arm-linux-gnueabi toolchain.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ StellarisWare installed. This saves you a 340MB download and various lengthly
 click-through EULAs.
  
 The BSD-licensed header file for the Launchpad's microprocessor (LM4F120H5QR)
-is includedin this project. All you need is the flash tool (lm4tools) and a
+is included in this project. All you need is the flash tool (lm4tools) and a
 compiler and there's a shell script that will fetch both of those for you.
 
 Thanks must go to
--- prerequisities.sh
+++ prerequisities.sh
@@ -1,6 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get install gcc-arm-linux-gnueabi libusb-1.0.0-dev
-git clone https://github.com/utzig/lm4tools.git
-cd lm4tools/lm4flash
-make
+# This will fetch Ubuntu's Linux cross-compiler. It works fine for basic bare-metal compilation.
+
+fetch_ubuntu_compiler()
+{
+	echo Installing gcc-arm-linux-gnueabi using apt-get
+	sudo apt-get install gcc-arm-linux-gnueabi
+}
+
+# This will fetch a pre-build ARM toolchain designed for the Cortex-M4
+
+fetch_arm_compiler()
+{
+	wget https://launchpad.net/gcc-arm-embedded/4.6/4.6-2012-q4-update/+download/gcc-arm-none-eabi-4_6-2012q4-20121016.tar.bz2
+	tar xvjf gcc-arm-none-eabi-4_6-2012q4-20121016.tar.bz2
+}
+
+# This will fetch and install lm4tools so we can flash the Launchpad
+fetch_lm4tools()
+{
+	echo Installing libusb-1.0.0-dev using apt-get
+	sudo apt-get install libusb-1.0.0-dev
+	echo Fetching lm4tools
+	git clone https://github.com/utzig/lm4tools.git
+	cd lm4tools/lm4flash
+	echo Building lm4tools
+	make
+}
+
+
+#fetch_ubuntu_compiler
+fetch_arm_compiler
+fetch_lm4tools
